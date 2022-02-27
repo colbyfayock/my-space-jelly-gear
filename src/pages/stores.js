2939f2e471527d6b99db42c28d16375c76237eeb
colbyfayock <fayock@gmail.com>
2022-02-27 16:50:27 -0500

    centering map on markers and using active store location to zoom in on click

--- src/pages/stores.js
+++ src/pages/stores.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head'
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import {
@@ -5,6 +6,8 @@ import {
   InMemoryCache,
   gql
 } from "@apollo/client";
+import center from '@turf/center';
+import { points } from '@turf/helpers';
 
 import Layout from '@components/Layout';
 import Container from '@components/Container';
@@ -14,6 +17,14 @@ import Map from '@components/Map';
 import styles from '@styles/Page.module.scss'
 
 export default function Stores({ storeLocations }) {
+  const [activeStore, setActiveStore] = useState();
+
+  const features = points(storeLocations.map(({ location }) => {
+    return [location.latitude, location.longitude]
+  }));
+
+  const [defaultLatitude, defaultLongitude] = center(features)?.geometry.coordinates;
+
   return (
     <Layout>
       <Head>
@@ -29,6 +40,9 @@ export default function Stores({ storeLocations }) {
           <div className={styles.storesLocations}>
             <ul className={styles.locations}>
               {storeLocations.map(location => {
+                function handleOnClick() {
+                  setActiveStore(location.id);
+                }
                 return (
                   <li key={location.id}>
                     <p className={styles.locationName}>
@@ -41,7 +55,7 @@ export default function Stores({ storeLocations }) {
                       { location.phoneNumber }
                     </p>
                     <p className={styles.locationDiscovery}>
-                      <button>
+                      <button onClick={handleOnClick}>
                         View on Map
                       </button>
                       <a href={`https://www.google.com/maps/dir//${location.location.latitude},${location.location.longitude}/@${location.location.latitude},${location.location.longitude},12z/`} target="_blank" rel="noreferrer">
@@ -58,10 +72,19 @@ export default function Stores({ storeLocations }) {
 
           <div className={styles.storesMap}>
             <div className={styles.storesMapContainer}>
-              <Map className={styles.map} center={[0, 0]} zoom={2} scrollWheelZoom={false}>
+              <Map className={styles.map} center={[defaultLatitude, defaultLongitude]} zoom={4} scrollWheelZoom={false}>
                 {({ TileLayer, Marker, Popup }, map) => {
+                  const MapEffect = () => {
+                    useEffect(() => {
+                      if ( !activeStore ) return;
+                      const { location } = storeLocations.find(({ id }) => id === activeStore);
+                      map.setView([location.latitude, location.longitude], 14)
+                    }, [activeStore])
+                    return null;
+                  }
                   return (
                     <>
+                      <MapEffect />
                       <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -69,7 +92,7 @@ export default function Stores({ storeLocations }) {
                       {storeLocations.map(location => {
                         const { latitude, longitude } = location.location;
                         return (
-                          <Marker position={[latitude, longitude]}>
+                          <Marker key={location.id} position={[latitude, longitude]}>
                             <Popup>
                               <p>{ location.name }</p>
                               <p>{ location.address }</p>
